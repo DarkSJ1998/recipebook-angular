d4d1a9af4a62581a0fc7f0cc16720dc3049409df
DarkSJ1998 <sarthakjn12@gmail.com>
2020-10-08 19:24:51 +0530

    Shopping List section components added

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,13 @@
+export class Ingredient {
+	// TypeScript Shortcut way
+	constructor(public name: string, public amount: number) {}
+
+	// Standard way
+	// public name: string;
+	// public amount: number;
+
+	// constructor(name: string, amount: number) {
+	// 	this.name = name;
+	// 	this.amount = amount;
+	// }
+}
